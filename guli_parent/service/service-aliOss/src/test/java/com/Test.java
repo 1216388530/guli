99dc706e402f5fa066c2a13aba769f57c39ba39d
1216388530 <39817595+1216388530@users.noreply.github.com>
2020-07-26 10:45:36 +0800

    改阿里云
    。。。

--- guli_parent/service/service-aliOss/src/test/java/com/Test.java
+++ guli_parent/service/service-aliOss/src/test/java/com/Test.java
@@ -7,19 +7,12 @@ import com.aliyun.oss.OSSClientBuilder;
 public class Test {
 
     public void test(){
-        /**
-         * aliyun.oss.file.endpoint=oss-cn-beijing.aliyuncs.com
-         *     aliyun.oss.file.keyid=LTAI4GG73NYihRyA9JXgK7H5
-         *     aliyun.oss.file.keysecret=YUo2S6Q37LD0patoC24OcZFM6MVrY9
-         *     bucket可以在控制台创建，也可以使用java代码创建
-         *     aliyun.oss.file.bucketname=edu-1216388530-0101
-         *
-         */
-        String endpoint = "oss-cn-beijing.aliyuncs.com";
-        String accessKeyId = "LTAI4GG73NYihRyA9JXgK7H5";
-        String accessKeySecret = "YUo2S6Q37LD0patoC24OcZFM6MVrY9";
-        String bucketName = "edu-1216388530-0101";
-        String objectName = "2020/06/05/3db457cae083428288ceb1a82aa09a9f.jpg";
+        
+        String endpoint = "自己建";
+        String accessKeyId = "自己建";
+        String accessKeySecret = "自己建";
+        String bucketName = "自己建";
+        String objectName = "自己建";
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
         ossClient.deleteObject(bucketName, objectName);
@@ -27,7 +20,7 @@ public class Test {
     }
     @org.junit.Test
     public void test2(){
-        String si ="https://edu-1216388530-0101.oss-cn-beijing.aliyuncs.com/2020/06/05/68a13c71c2f24120ad550aea9775f998.png";
+        String si ="自己建";
         System.out.println(si.substring(si.indexOf("/",10)+1,si.length()));
     }
 }
